9bbfb864b754569a24a2a5e513606ab5fad6ece7
Emmanuel Ronoh <eronoh036@gmail.com>
2025-04-27 17:22:44 +0300

    added base to config files

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,75 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: {
+      // These are needed for Web3/walletconnect compatibility
+      util: 'rollup-plugin-node-polyfills/polyfills/util',
+      sys: 'util',
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      path: 'rollup-plugin-node-polyfills/polyfills/path',
+      querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+      punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+      url: 'rollup-plugin-node-polyfills/polyfills/url',
+      string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
+      http: 'rollup-plugin-node-polyfills/polyfills/http',
+      https: 'rollup-plugin-node-polyfills/polyfills/http',
+      os: 'rollup-plugin-node-polyfills/polyfills/os',
+      assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+      constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+      _stream_duplex: 'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+      _stream_passthrough: 'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+      _stream_readable: 'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+      _stream_writable: 'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+      _stream_transform: 'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+      crypto: 'crypto-browserify',
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
+      // Node.js global to browser globalThis
       define: {
-        global: 'globalThis'
+        global: 'globalThis',
       },
+      // Enable esbuild polyfill plugins
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  }
-})
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        rollupNodePolyFill(),
+      ],
+      output: {
+        manualChunks: {
+          // Split large dependencies into separate chunks
+          ethers: ['ethers'],
+          walletconnect: ['@walletconnect/client'],
+          react: ['react', 'react-dom'],
+          reactRouter: ['react-router-dom'],
+          framer: ['framer-motion'],
+        },
+      },
+      chunkSizeWarningLimit: 1500, // Increase chunk size warning limit
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true, // Required for some Web3 dependencies
+    },
+  },
+});
